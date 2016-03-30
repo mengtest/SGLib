539a3feb5d3e0914d9884fe4c7f6f7b2cc68d709
wangyankai <wuxikq@gmail.com>
2016-03-30 23:17:09 +0800

    UI 库合并过来

---
+++ Assets/Supergood/UI/CommonUI/Action/BaseClass/ActionCallBack.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class ActionCallBack
+{
+
+	public TweenCallback OnCompleteMethod;
+	public TweenCallback OnStartMethod;
+	public TweenCallback OnStepCompleteMethod;
+
+	public ActionCallBack ()
+	{
+	}
+
+	public void SetCompleteMethod (System.Action a)
+	{
+		if (a != null)
+			OnCompleteMethod = new TweenCallback (a);
+	}
+	
+	public void SetStartMethod (System.Action a)
+	{
+		if (a != null)
+			OnStartMethod = new TweenCallback (a);
+	}
+	
+	public void SetStepCompleteMethod (System.Action a)
+	{
+		if (a != null)
+			OnStepCompleteMethod = new TweenCallback (a);
+	}
+
+	public void AddCompleteMethod (System.Action a)
+	{
+		if (a != null)
+			OnCompleteMethod += new TweenCallback (a);
+	}
+
+	public void AddStartMethod (System.Action a)
+	{
+		if (a != null)
+			OnStartMethod += new TweenCallback (a);
+	}
+
+	public void AddStepCompleteMethod (System.Action a)
+	{
+		if (a != null)
+			OnStepCompleteMethod += new TweenCallback (a);
+	}
+
+
+}
